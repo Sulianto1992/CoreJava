3d0a54ff5f17e50804f234ab0f2a244e8dc5a377
Sulianto1992 <Sulianto1992@gmail.com>
2019-07-30 06:52:42 +0800

    Commot Done, 0652HRS, 30072019

---
+++ src/coreJavaAssignment/Problem1.java
@@ -0,0 +1,20 @@
+package coreJavaAssignment;
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+		
+		
+		for (int i = 0;i < 4; i++)
+		{
+			System.out.println(" ");
+			for (int j = 0; j <= i; j++)
+			{
+				System.out.print("* ");				
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
