c894a7a0790dbd289a14c4dd014dfba53615dfd8
Sulianto1992 <Sulianto1992@gmail.com>
2019-07-31 04:50:16 +0800

    Commit Done, 31072019, 0450HRS

--- src/coreJavaAssignment/Problem1.java
+++ src/coreJavaAssignment/Problem1.java
@@ -4,10 +4,10 @@ public class Problem1 {
 
 	public static void main(String[] args) {
 		
-		
 		for (int i = 0;i < 4; i++)
 		{
 			System.out.println(" ");
+			
 			for (int j = 0; j <= i; j++)
 			{
 				System.out.print("* ");				
