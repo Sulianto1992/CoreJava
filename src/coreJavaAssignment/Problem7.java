c8c3684e8e42cd6e5822145a3957563a767c33af
sulianto.sulaiman <sulianto.sulaiman@TRAINING016.optimum.net>
2019-07-31 17:59:59 +0800

    Commit Done, 31073019, 1800 HRS

---
+++ src/coreJavaAssignment/Problem7.java
@@ -0,0 +1,18 @@
+package coreJavaAssignment;
+
+public class Problem7 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j <= i; j = j + 2)
+			{
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
