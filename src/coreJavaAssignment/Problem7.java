4f519475382a0a37f6323f522de9102971a6461a
Sulianto1992 <Sulianto1992@gmail.com>
2019-08-02 05:28:50 +0800

    Commit Done, 02082019, 0530HRS

--- src/coreJavaAssignment/Problem7.java
+++ src/coreJavaAssignment/Problem7.java
@@ -4,14 +4,29 @@ public class Problem7 {
 
 	public static void main(String[] args) {
 		
-		int i, j;
-		for (i = 1; i <= 5; i++)
+		int numberOfRows = 5;
+		
+		
+		for (int i = 1; i <= numberOfRows; i++)
 		{
-			for (j = 1; j <= (i*2 - 1); j++)
+			for (int j = i; j <= numberOfRows; j++)
 			{
 				System.out.print(j + " ");
+				
+				if (i == 1)
+				{
+					break;
+				}
 			}
-			System.out.println("");
+			
+			/*
+			for (int k = numberOfRows - 1 ; k >= i; k--)
+			{
+				System.out.print(k + " ");
+			}
+			System.out.println();
+			*/
+			System.out.println();
 		}
 
 	}
