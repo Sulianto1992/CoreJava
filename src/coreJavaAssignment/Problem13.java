e1f764d0667aeb52d737824377fbd982c8e6e776
Sulianto1992 <Sulianto1992@gmail.com>
2019-08-03 17:13:26 +0800

    COMMIT DONE, 03082019, 1713HRS

--- src/coreJavaAssignment/Problem13.java
+++ src/coreJavaAssignment/Problem13.java
@@ -1,15 +1,14 @@
 package coreJavaAssignment; //Name of the package
 
-public class Problem13 
+public class Problem13 //Name of the class
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) //Start of the program
 	{
-		
 		//Array declaration
 		int[] arr = {20, 20, 30, 40, 50, 50, 50};
 	    int arrayLength = arr.length;
 	    
-	    //For loop
+	    //Execute this statement as long as it is true
 	    for (int a = 0; a < arrayLength; a++)
 	    {
 	    	for (int b = a + 1; b < arrayLength; b++)
