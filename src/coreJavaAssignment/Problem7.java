8dacc4554b3e21f85f8774f805f8c809f6210894
Sulianto1992 <Sulianto1992@gmail.com>
2019-08-03 17:32:47 +0800

    COMMIT DONE, 03082019, 1732HRS

--- src/coreJavaAssignment/Problem7.java
+++ src/coreJavaAssignment/Problem7.java
@@ -1,15 +1,15 @@
-package coreJavaAssignment;
+package coreJavaAssignment; //Name of the package
 
-public class Problem7 {
-
-	public static void main(String[] args) {
-		
+public class Problem7 //Name of the class 
+{
+	public static void main(String[] args) //Start of the program
+	{
 		//Variable Declaration
 		int count = 1;
 		int temp = 0;
 		int temp2 = 0;
 		
-		//For loop
+		//Execute this statement as long as it is true
 		for (int i = 1; i <= 5; i++)
 		{
 			for (int j = 1; j <= i; j++) 
@@ -18,7 +18,7 @@ public class Problem7 {
 				{
 					System.out.print(i + " ");
 					count+=2;
-				}
+				} 
 				else if (j < i)
 				{
 					if (j == 1)
@@ -39,11 +39,14 @@ public class Problem7 {
 					{
 						System.out.print(l + " ");
 					} //end for
+					
 					count+=2;
+					
 				} //end if else
 			
 			} //end for
 			System.out.println(); //Go to the next line 
+			
 		} //end for
 	} //end main
 } //end Problem 7
