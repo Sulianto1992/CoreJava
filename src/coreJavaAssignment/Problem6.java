d69d62dc6c72173fd4d30ba7d9b774df5a8d4a49
Sulianto1992 <Sulianto1992@gmail.com>
2019-08-03 17:36:28 +0800

    COMMIT DONE, 03082019, 1736HRS

--- src/coreJavaAssignment/Problem6.java
+++ src/coreJavaAssignment/Problem6.java
@@ -1,9 +1,11 @@
-package coreJavaAssignment;
+package coreJavaAssignment; //Name of the package
 
-public class Problem6 
+public class Problem6 //Name of the class
+{
+    public static void main(String[] args) //Start of the program
     {
-    public static void main(String[] args) {
 	
+    	//Execute this statement as long as it is true
     	for (int i = 0; i <= 4; i++)
 		{
 			System.out.println(" ");
@@ -11,14 +13,16 @@ public class Problem6
 			for (int j = 0; j <= i; j++)
 			{
 				System.out.print("* ");	
+				
 				if (j >= 2)
 				{
 					System.out.print("* ");	
-				}
-			}
-			System.out.println(" ");
+				} //end if 
+				
+			} //end for
+			
+			System.out.println(); //
 		} //end for loop
 
 	} //end main
-
-}
+} //end Problem6
