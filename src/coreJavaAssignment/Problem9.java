0da82918eeee4b4157eb58f7c9e357e1cb7ff2a3
Sulianto1992 <Sulianto1992@gmail.com>
2019-08-03 17:27:45 +0800

    Commit Done, 03082019, 1727HRS

--- src/coreJavaAssignment/Problem9.java
+++ src/coreJavaAssignment/Problem9.java
@@ -1,14 +1,18 @@
-package coreJavaAssignment;
+package coreJavaAssignment; //Name of the package
 
-import java.util.stream.IntStream;
+import java.util.stream.IntStream; //Import stream from Java Library
 
-public class Problem9 {
-
-	public static void main(String[] args) 
+public class Problem9 //Name of the class
+{
+	public static void main(String[] args) //Start of the program
 	{
 	    //Array declaration
 		int [] number = {100, 67, 11, -87, 26};
 		int numberToFind = 100;
+		
+		//Display the array and the element to be found 
+		System.out.println("Array: 100, 67, 11, -87, 26");
+		System.out.println("Number to find: 100");
 		
 		//Determine whether the number can be found in the array
 		boolean found = IntStream.of(number).anyMatch(n -> n == numberToFind);
@@ -16,11 +20,12 @@ public class Problem9 {
 		//Output the message 
 		if (found)
 		{
-			System.out.println("Found");	
+			System.out.println("Output: Found");	
 		}
 		else
 		{
-			System.out.println("Not Found");
+			System.out.println("Output: Not Found");
 		} //end if
+		
 	} //end main
 } //end Problem9
