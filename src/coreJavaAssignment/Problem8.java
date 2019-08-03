009f5d3595f06a409308eb3567895a0c3ff684ad
Sulianto1992 <Sulianto1992@gmail.com>
2019-08-03 17:30:22 +0800

    COMMIT DONE, 03082019, 1730HRS

--- src/coreJavaAssignment/Problem8.java
+++ src/coreJavaAssignment/Problem8.java
@@ -1,12 +1,13 @@
-package coreJavaAssignment;
+package coreJavaAssignment; //Name of the package
 
-public class Problem8 {
-
-	public static void main(String[] args) {
-		
+public class Problem8 //Name of the class
+{
+	public static void main(String[] args) //Start of the program
+	{
 		//Variable Declaration
 		int a, b;
 		
+		//Execute this statement as long as it is true
 		for (a = 4; a >= 0; a--)
 		{
 			for (b = 0; b <= a; b++)
@@ -16,9 +17,10 @@ public class Problem8 {
 				if (b >= 2)
 				{
 					System.out.print("* ");
-				}
-			}
-			System.out.println();
+				} //end if
+			} //end for
+			
+			System.out.println(); //Move to the next line
 			
 			if (b > 2)
 			{
@@ -27,5 +29,4 @@ public class Problem8 {
 		} //end for 
 
 	} //end main
-
 } //end Problem8
